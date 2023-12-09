0f7bf02a638899689af06bc97da061806743dc14
Faizan Rasool <faizanraso@outlook.com>
2023-12-08 22:57:11 -0500

    ui improvements

--- app/_components/AuthCard.tsx
+++ app/_components/AuthCard.tsx
@@ -14,9 +14,9 @@ import Link from "next/link";
 
 export default function AuthCard() {
   return (
-    <Card className="px-8 py-5 shadow-inner">
+    <Card className="px-8 py-5 bg-gray-900 border-gray-900 shadow-xl shadow-gray-950">
       <CardHeader className="space-y-1">
-        <CardTitle className="text-lg font-bold flex flex-row gap-x-2 pb-2">
+        <CardTitle className="text-lg font-bold flex flex-row gap-x-2 pb-2 text-gray-100 ">
           PR BUDDY{" "}
           <svg
             width="24px"
@@ -25,46 +25,46 @@ export default function AuthCard() {
             stroke-width="1.5"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            color="#000000"
+            className="stroke-gray-100"
           >
             <path
               d="M18 21C19.1046 21 20 20.1046 20 19C20 17.8954 19.1046 17 18 17C16.8954 17 16 17.8954 16 19C16 20.1046 16.8954 21 18 21Z"
-              stroke="#000000"
               stroke-width="1.5"
               stroke-linecap="round"
               stroke-linejoin="round"
+              className="stroke-gray-100"
             ></path>
             <path
               d="M6 21C7.10457 21 8 20.1046 8 19C8 17.8954 7.10457 17 6 17C4.89543 17 4 17.8954 4 19C4 20.1046 4.89543 21 6 21Z"
-              stroke="#000000"
               stroke-width="1.5"
               stroke-linecap="round"
               stroke-linejoin="round"
+              className="stroke-gray-100"
             ></path>
             <path
               d="M6 11V17"
-              stroke="#000000"
               stroke-width="1.5"
               stroke-linecap="round"
               stroke-linejoin="round"
+              className="stroke-gray-100"
             ></path>
             <path
               d="M18 17V7C18 7 18 5 16 5H12"
-              stroke="#000000"
               stroke-width="1.5"
               stroke-linecap="round"
               stroke-linejoin="round"
+              className="stroke-gray-100"
             ></path>
             <path
               d="M4 7.24264L6.12132 5.12132M6.12132 5.12132L8.24264 3M6.12132 5.12132L4 3M6.12132 5.12132L8.24264 7.24264"
-              stroke="#000000"
               stroke-width="1.5"
               stroke-linecap="round"
               stroke-linejoin="round"
+              className="stroke-gray-100"
             ></path>
           </svg>
         </CardTitle>
-        <CardDescription className="text-xs">
+        <CardDescription className="text-xs text-gray-200">
           Choose one of the sign in methods below to get started
         </CardDescription>
       </CardHeader>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import AuthCard from "./_components/AuthCard";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#23486F] via-[#192532] to-[#10131C]">
       <AuthCard />
     </main>
   );
