603bec107f6feb01e1e547767216862fa533dd9f
Faizan Rasool <faizanraso@outlook.com>
2023-12-12 18:59:13 -0500

    ui improvement

--- app/_components/generatePage/GenerateDescription.tsx
+++ app/_components/generatePage/GenerateDescription.tsx
@@ -1,17 +1,19 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 
 import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Loader2 } from "lucide-react";
+import PRIcon from "../icons/PRIcon";
 
 export default function GenerateDescription() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between">
-        <div className="w-5/6">
+        <div className="w-full">
           <Input
             placeholder="OpenAI API Key"
             className="bg-[#192532] border-0 text-gray-300 font-medium"
@@ -19,10 +21,17 @@ export default function GenerateDescription() {
           />
         </div>
         <div className="pl-4">
-          <Button disabled>
-            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-            Please wait
-          </Button>
+          {!isLoading ? (
+            <Button className="bg-[#192532] hover:bg-[#23486F] transition duration-150 font-bold text-xs w-32 gap-x-2 items-center justify-center">
+              Generate
+              <PRIcon className="w-[10px]" />
+            </Button>
+          ) : (
+            <Button disabled className="font-bold text-xs w-32">
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Please wait
+            </Button>
+          )}
         </div>
       </div>
       <div className="pt-6 pb-10">
