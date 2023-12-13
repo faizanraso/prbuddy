d3ce8d357f64249daf3e930a86f415b77a110d38
Faizan Rasool <faizanraso@outlook.com>
2023-12-12 20:13:04 -0500

    main page ui complete

--- app/_components/generatePage/GenerateDescription.tsx
+++ app/_components/generatePage/GenerateDescription.tsx
@@ -1,15 +1,41 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
-import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Loader2 } from "lucide-react";
 import PRIcon from "../icons/PRIcon";
+import CheckIcon from "../icons/CheckIcon";
+import CopyIcon from "../icons/CopyIcon";
 
 export default function GenerateDescription() {
+  const [copied, setCopied] = useState<boolean>(false);
+  const [response, setResponse] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  function handleCopy() {
+    setCopied(true);
+    navigator.clipboard.writeText(response);
+    toast.success("Copied!", {
+      style: {
+        borderRadius: "10px",
+        background: "#10331D",
+        color: "#fff",
+      },
+    });
+  }
+
+  useEffect(() => {
+    if (!copied) return;
+    const timer = setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [copied]);
+
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between">
@@ -22,24 +48,29 @@ export default function GenerateDescription() {
         </div>
         <div className="pl-4">
           {!isLoading ? (
-            <Button className="bg-[#192532] hover:bg-[#23486F] transition duration-150 font-bold text-xs w-32 gap-x-2 items-center justify-center">
+            <Button className="bg-[#192532] hover:bg-[#23486F] transition duration-150 font-bold text-sm w-32 gap-x-2 items-center justify-center">
               Generate
               <PRIcon className="w-[10px]" />
             </Button>
           ) : (
-            <Button disabled className="font-bold text-xs w-32">
+            <Button disabled className="font-bold text-sm w-32">
               <Loader2 className="mr-2 h-4 w-4 animate-spin" />
               Please wait
             </Button>
           )}
         </div>
       </div>
-      <div className="pt-6 pb-10">
-        <Textarea
-          placeholder={"longMessage"}
-          disabled
-          className="bg-[#192532] border-0 h-64"
-        />
+      <div className="pt-6 pb-10 relative">
+        <button
+          className="absolute top-0 right-0 mt-8 mr-4"
+          onClick={handleCopy}
+          disabled={copied}
+        >
+          {!copied ? <CopyIcon /> : <CheckIcon />}
+        </button>
+        <pre className="bg-[#192532] rounded-lg border-0 h-64 p-4 overflow-scroll text-gray-300">
+          {response}
+        </pre>
       </div>
     </div>
   );
