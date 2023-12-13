5a89b81fc657a5379b6f3ede60ea45b147b4bbe4
Faizan Rasool <faizanraso@outlook.com>
2023-12-13 18:36:35 -0500

    redeploy

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,10 +1,6 @@
 import { NextRequest } from "next/server";
 import { OpenAIStream, OpenAIStreamPayload } from "../../../utils/OpenAIStream";
 
-if (!process.env.OPENAI_API_KEY) {
-  throw new Error("Missing env var from OpenAI");
-}
-
 export const runtime = "edge";
 
 export async function POST(req: NextRequest) {
